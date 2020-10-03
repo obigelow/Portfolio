f5da6d5be9c1d2657dfeb31cf5befe6b4e954b6d
Oliver Bigelow <oelucy@gmail.com>
2020-10-03 14:03:53 -0700

    started working on header an vavbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from "./components/NavBar"
+import Header from "./components/Header"
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
